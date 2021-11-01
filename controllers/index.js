abf623c154347a5dbd94eed2f0d86e12cc800247
Brigitte Powers PC <reyhn701@gmail.com>
2021-11-01 16:06:17 -0500

    Added controllers and routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
-// TODO: an example of index.js controller
-
 const router = require("express").Router();
-const homeRoutes = require("./homeRoutes");
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
 
+router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
